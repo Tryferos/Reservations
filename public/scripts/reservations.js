309f5f381927d7e4c5691c0089730667d4bdfb99
Tryferos <truf022@gmail.com>
2023-05-29 14:02:06 +0300

    add reservations page, and display a reservation information

--- public/scripts/reservations.js
+++ public/scripts/reservations.js
@@ -25,6 +25,18 @@ async function addReservation(stadium, reservation){
     const list = document.getElementById("reservations-list");
     const li = document.createElement("li");  
     li.setAttribute('class', 'reservation-item');
+    const d = new Date().getTime();
+    const currentDate= d + 24*60*60*1000 + 59*60*1000 + 59*1000;
+    if(reservation.date_day>currentDate){
+        li.setAttribute('data-past', 'false')
+        if((reservation.date_day-currentDate)<(24*60*60*1000)){
+            li.setAttribute('data-soon', 'true')
+        }else{
+            li.setAttribute('data-soon', 'false')
+        }
+    }else{
+        li.setAttribute('data-past', 'true')
+    }
     const fig = document.createElement("figure");
     const img = document.createElement("img");
     img.setAttribute('src', stadium.image);
@@ -37,28 +49,45 @@ async function addReservation(stadium, reservation){
     const div = document.createElement("div");
     div.setAttribute('class', 'reservation-info');
     const p = document.createElement("p");
-    p.innerHTML = `<span>Reservation date:</span> <date>${reservation.date}</date>`;
+    const date = getReservationDate(stadium, reservation);
+    p.innerHTML = `<span>Reservation date:</span> <time>${formatDate(date, stadium.game_length)}</time>`;
     const p2 = document.createElement("p");
-    const formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'EUR',
-    });
-    p2.innerText = `${stadium.sport} - ${stadium.type} - ${formatter.format(stadium.price_total)}€`;
+    p2.innerText = `${stadium.sport} - ${stadium.type} - ${formatPrice(stadium.price_total)}€`;
     div.appendChild(p2);
     div.appendChild(p);
     li.appendChild(div);
     list.appendChild(li);
 }
 
-function formatDate(date){
+function formatPrice(price){
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'EUR',
+    }).format(price);
+}
+
+function getReservationDate(stadium, reservation){
+    let date = new Date(reservation.date_day).getTime();
+    date -= 23*60*60*1000;
+    date -= 59*60*1000;
+    date -= 59*1000;
+    date += stadium.available_from*60*60*1000;
+    date += (stadium.game_length*reservation.time_slot)*60*1000;
+    return date
+    
+}
+
+function formatDate(date, game_length){
     const d = new Date(date);
     const year = d.getFullYear();
     const month = d.getMonth()+1;
     const day = d.getDate();
-    const hours = d.getHours();
-    const minutes = d.getMinutes();
-    const seconds = d.getSeconds();
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    const hours = `${d.getHours()}`.length==1 ? `0${d.getHours()}` : `${d.getHours()}`;
+    const minutes = `${d.getMinutes()}`.length==1 ? `0${d.getMinutes()}` : `${d.getMinutes()}`;
+    const endDate = new Date(date+(game_length*60*1000));
+    const ehours = `${endDate.getHours()}`.length==1 ? `0${endDate.getHours()}` : `${endDate.getHours()}`;
+    const eminutes = `${endDate.getMinutes()}`.length==1 ? `0${endDate.getMinutes()}` : `${endDate.getMinutes()}`;
+    return `${day}/${month}/${year}, ${hours}:${minutes}-${ehours}:${eminutes}`;
 }
 
 
